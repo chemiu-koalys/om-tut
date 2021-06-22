104a364a22921493602a237a66cafffda49445e1
Nechemya Ungar <chemiu@koalys.com>
2021-06-22 13:28:22 +0300

    some style changes

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -4,47 +4,54 @@
             [om.dom :as dom :include-macros true]
             [cljs.core.async :refer [<! chan timeout put!]]
             [cljs.core.async :refer [chan pub]]
-            [cljs-http.client :as http] [clojure.string :as string]))
+            [cljs-http.client :as http] 
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
+(defmacro dbg [x]
+  `(let [x# ~x]
+     (when (audyx.app.config-logic/get-in [:general :enable-debugging])
+       (print "dbg-key" '~x ":")
+       (console/log "%cdbg-value: %o" "color:hsl(208, 53%, 58%)" x#))
+     x#))
+
+(def results-per-page 30)
+
 (defn console
   "This is a wrapper function to call js console methods  
-   https://developer.mozilla.org/en-US/docs/Web/API/Console  
-   "
+   https://developer.mozilla.org/en-US/docs/Web/API/Console"
   ([whatever]
-   (js/console.log (clj->js whatever)))
-  ([method whatever]
+   (js/console.log (console whatever "log")))
+  ([whatever method]
    ((aget js/console method) (clj->js whatever))))
 
 (defn on-js-reload [])
 
 (defonce app-state (atom {:page 1 :table-data nil}))
 
-(defn reset [owner]
+(defn reset-state! [owner]
   (om/set-state! owner {:search-term "" :table-data nil}))
 
+(defn count-pages [total-count] (Math/ceil (/ total-count results-per-page)))
 
-(def url "https://api.github.com/search/code")
-(def auth-header {"Authorization" "Basic Y2hlbWl1LWtvYWx5czpnaHBfNmVRWjF6dnBEVmdVTk1BbVJoNk80d2htbFhEZUYzMk9vakx2"})
-
-(defn getResaults [owner search-term page]
-  (go (let [response (<! (http/get url
-                                   {:with-credentials? false
-                                    :headers           auth-header
-                                    :query-params      {:q (str search-term " extension:cljs") :page (if (> page 1) page 1)}}))]
-        ;; (fn [] (put! (result-channel)  (response)))
-        (om/set-state! owner {:table-data (conj (:body response) {:pages (Math/ceil (/ (:total_count (:body response)) 30))})})
+(defn fetch-results [owner search-term page]
+  (let [url "https://api.github.com/search/code"
+        auth-header {"Authorization" "Basic Y2hlbWl1LWtvYWx5czpnaHBfNmVRWjF6dnBEVmdVTk1BbVJoNk80d2htbFhEZUYzMk9vakx2"}
+        q (str search-term " extension:cljs")
+        page (if (> page 1) page 1)
+        options {:with-credentials? false
+                 :headers auth-header
+                 :query-params {:q q :page page}}
+        ]
+    (go (let [response (<! (http/get url options))]
+        (let [data (:body response)]
+          (om/set-state! owner {:table-data (conj data {:pages (count-pages (:total_count data))})}))
         (console "state changed")
-        (console (om/get-state owner))
-      ;;   (fn [] (put! (result-channel)  (conj data {:pages (Math/ceil (/ (:total_count data) 30))})))
-        ;; (console "result-channel")
-        ;; (console result-channel)
-        )))
+        (console (om/get-state owner))))))
 
-
-(defn handle-search-state [owner val]
-  (reset owner)
+(defn handle-search-state! [owner val]
+  (reset-state! owner)
   (om/set-state! owner {:search-term val}))
 
 ;; how can this be stateless and how can I incorporate the state method inside the component
@@ -55,33 +62,40 @@
       {:search-term ""})
     om/IRenderState
     (render-state [_ {:keys [search-term]}]
-      (dom/div nil
-               (dom/label #js {:className "label1" :htmlFor (:field-name data)} (:field-label data))
-               (dom/input #js {:className (:class data) :id (:field-name data)
-                               :onChange  (fn [e] (handle-search-state owner e.target.value) )})
-               (dom/button #js {:onClick
-                                (fn [] (put! (:search-channel data) search-term))}
-                           "Execute search!")))))
+      (dom/div nil 
+               (dom/label #js {:className "label1" 
+                               :htmlFor (:field-name data)}
+                          (:field-label data))
+      (dom/input #js {:className (:class data) 
+                               :id (:field-name data) 
+                               :onChange  #(handle-search-state! owner (-> % .-target .-value))})
+      (dom/button #js {:onClick  
+                       (fn [] (put! (:search-channel data) search-term))} 
+                   "Execute search!")))))
 
 (defn search-results-component "This component renders the search results" [data owner]
   (reify
     om/IInitState
     (init-state [_]
-      {:page 1 :table-data [] :result-channel (chan)})
+      {:page 1 
+       :table-data [] 
+       :result-channel (chan)})
     om/IWillMount
     (will-mount [_]
-      (getResaults owner (:search-term data) (om/get-state owner :page))
-      (console "table" data))
+      (fetch-results owner (:search-term data) (om/get-state owner :page))
+      (console data "table"))
     om/IRenderState
     (render-state [_ {:keys [table-data]}]
       (dom/div nil
-               (dom/h2 #js {:className "results-header"} (str "Search results for " (:search-term data)))
+               (dom/h2 #js {:className "results-header"} 
+                       (str "Search results for " (:search-term data)))
                (if (empty? table-data)
                  (dom/div #js {:className "loading-message"} (str "Loading ..."))
-                 (dom/div nil 
-                          (dom/h3 #js {:className "results-count"} (str "Results Count " (:total_count table-data)))
-                          (map (dom/div #js {:className "row-item"} (str "file name " ((:name %))))(:items table-data))
-                          ))))))
+                 (dom/div nil
+                          (dom/h3 #js {:className "results-count"} 
+                                  (str "Results Count " (:total_count table-data)))))))))
+                          ;; (map (dom/div #js {:className "row-item"} (str "Results Count " (:name %)))(:items table-data))
+                          
 
 
 (defn widget "main app component" [data owner]
@@ -97,7 +111,7 @@
           (let [term (<! search-term)]
             (console (str "from chan " term))
             (om/set-state! owner :search-term term)
-            ;; (getResaults owner term (om/get-state owner :page))
+            ;; (fetch-results owner term (om/get-state owner :page))
             (recur)))))
     om/IWillUnmount
     (will-unmount [_]
@@ -108,7 +122,11 @@
       (console "Render")
       (dom/div nil
                (dom/h2 nil "Search github for cljs exetnsions")
-               (om/build search-component {:field-name "searchTerm" :field-label "Search Term" :class "input1" :search-channel search-channel})
+               (om/build search-component {
+                                           :field-name "searchTerm" 
+                                           :field-label "Search Term" 
+                                           :class "input1" 
+                                           :search-channel search-channel})
                (when-not (= search-term "") (om/build search-results-component  {:search-term search-term}))))))
 
 
